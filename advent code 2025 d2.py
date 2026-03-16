12f574f6b04400b82cff035d5eb7e6873a3f1172
Riley <dasher1505@gmail.com>
2026-03-16 20:07:19 +1300

    commenting

--- advent code 2025 d2.py
+++ advent code 2025 d2.py
@@ -2,6 +2,10 @@
 #I'm confident you could do this with substring shenanigans
 #I unknowingly coded the P2 solution for P1 (I didn't read properly)
 #Pretty convenient for me overall, since I just had to modify my code back to it's old state!
+#improvements for p1:
+#only need to check numbers with an even amount of digits
+#only need to check the middle split of those numbers
+#(inefficiencies from bandaiding what ended up P2's solution show up really hard)
 
 def main():
     codes = read_file(r"C:\Users\dashe\Local Documents\Personal Projects\advent_code\adventcode_d2.txt")
@@ -45,12 +49,12 @@ def iterate_each_num_p2(line):
 
 
 def check_for_pattern_p1(number):
-    str_number = str(number)
-    for i in range(1, len(str_number) // 2+1):
-        if len(str_number) % i == 0:
-            split_num = [str_number[j:j+i] for j in range(0, len(str_number), i)]
+    str_number = str(number) #str number useful so make it now
+    for i in range(1, len(str_number) // 2+1): #ensure we only check numbers we have to
+        if len(str_number) % i == 0: #extra check to make sure we're splitting number by a divisble amount (probably useless)
+            split_num = [str_number[j:j+i] for j in range(0, len(str_number), i)] #make list from equal 'sections' of num
             # print(f"{split_num}, which was {str_number}.")
-            if all(num == split_num[0] for num in split_num) and len(split_num) == 2:
+            if all(num == split_num[0] for num in split_num) and len(split_num) == 2: #are all elements in the list equal?
                 # print(f"{split_num} is a repeated pattern in {str_number}.") 
                 return True
     # print(f"{number} doesn't have a pattern.")
@@ -62,9 +66,10 @@ def check_for_pattern_p2(number):
     str_number = str(number)
     for i in range(1, len(str_number) // 2+1):
         if len(str_number) % i == 0:
-            split_num = [str_number[j:j+i] for j in range(0, len(str_number), i)]
+            split_num = [str_number[j:j+i] for j in range(0, len(str_number), i)] #make list from equal 'sections' of num
             # print(f"{split_num}, which was {str_number}.")
-            if all(num == split_num[0] for num in split_num) and len(split_num):
+            #are all elements in the list equal?
+            if all(num == split_num[0] for num in split_num) and len(split_num): 
                 # print(f"{split_num} is a repeated pattern in {str_number}.") 
                 return True
     # print(f"{number} doesn't have a pattern.")
